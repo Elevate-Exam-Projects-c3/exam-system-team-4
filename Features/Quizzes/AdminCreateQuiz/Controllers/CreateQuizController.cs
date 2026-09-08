1a707162dc2f7ca6f7cc3009d3b82594fb5b7ed5
Rojena Shehata <rojenashehata@gmail.com>
2026-09-08 20:52:42 +0300

    feat(quizzes): add create quiz controller

---
+++ Features/Quizzes/AdminCreateQuiz/Controllers/CreateQuizController.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using exam_system.Features.Quizzes.AdminCreateQuiz.Commands;
+using exam_system.Features.Quizzes.AdminCreateQuiz.ViewModels;
+using exam_system.Features.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exam_system.Features.Quizzes.AdminCreateQuiz.Controllers
+{
+    [ApiController]
+    public class CreateQuizController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CreateQuizController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateQuiz([FromBody] CreateQuizViewModel viewModel,CancellationToken cancellationToken)
+        {
+            var requestResult=await _mediator.Send(new CreateQuizCommand(
+                                                               DiplomaId: viewModel.DiplomaId,
+                                                                Title: viewModel.Title,
+                                                                DurationMinutes:viewModel.DurationMinutes,
+                                                                Instructions:viewModel.Instructions,
+                                                                PassScore: viewModel.PassScore,
+                                                                MaxAttempts:viewModel.MaxAttempts                                                             
+                                                                ), cancellationToken);
+            return StatusCode(requestResult.StatusCode, requestResult);
+        }
+    }
+}
