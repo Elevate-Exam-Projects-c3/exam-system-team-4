ad8bd6f3ed419d9ff78112119e110f0165f7b006
Rojena Shehata <rojenashehata@gmail.com>
2026-09-09 04:06:04 +0300

    feat(quizzes): add admin update quiz feature

---
+++ Features/Quizzes/AdminUpdateQuiz/Validators/UpdateQuizValidator.cs
@@ -0,0 +1,40 @@
+﻿using exam_system.Features.Quizzes.AdminCreateQuiz.ViewModels;
+using exam_system.Features.Quizzes.AdminUpdateQuiz.ViewModels;
+using FluentValidation;
+
+namespace exam_system.Features.Quizzes.AdminCreateQuiz.Validators
+{
+    public class UpdateQuizValidator : AbstractValidator<UpdateQuizViewModel>
+    {
+        public UpdateQuizValidator()
+        {
+            RuleFor(quiz => quiz.Title)
+                .NotEmpty()
+                .Length(min: 3, max: 200).WithMessage("Title Length Must Be Between 3 and 200 characters");
+
+            RuleFor(quiz => quiz.Instructions)
+                .MaximumLength(2000).WithMessage("Instructions Length Mustn't Be Greater than 2000 Character")
+                .When(quiz => !string.IsNullOrWhiteSpace(quiz.Instructions));
+
+            RuleFor(quiz => quiz.DurationMinutes)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(quiz => quiz.PassScore)
+                .InclusiveBetween(1, 100);
+
+
+            RuleFor(quiz => quiz.MaxAttempts)
+                .GreaterThan(0)
+                .When(x => x.MaxAttempts.HasValue);
+
+            RuleFor(quiz => quiz.StartDate)
+           .NotEmpty();
+
+            RuleFor(quiz => quiz.EndDate)
+           .NotEmpty()
+           .GreaterThan(quiz => quiz.StartDate)
+           .WithMessage("End date must be after start date.");
+        }
+    }
+}
---
+++ Features/Quizzes/AdminUpdateQuiz/ViewModels/UpdateQuizViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace exam_system.Features.Quizzes.AdminUpdateQuiz.ViewModels
+{
+    public record UpdateQuizViewModel(
+                                  string Title,
+                                  int DurationMinutes,
+                                  DateTime StartDate,
+                                  DateTime EndDate,
+                                  string? Instructions,
+                                  int PassScore,
+                                  int? MaxAttempts);
+
+
+
+}
