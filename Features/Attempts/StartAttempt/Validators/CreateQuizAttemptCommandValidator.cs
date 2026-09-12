45a40e2449bc8fdada2853fe17ea94bf40efc3a2
Rojena Shehata <rojenashehata@gmail.com>
2026-09-13 00:50:52 +0300

    feat(attempt): add quiz attempt validation and creation

---
+++ Features/Attempts/StartAttempt/Validators/CreateQuizAttemptCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using exam_system.Common.Enums;
+using exam_system.Features.Attempts.StartAttempt.Commands;
+using FluentValidation;
+namespace exam_system.Features.Attempts.StartAttempt.Validators
+{
+    public class CreateQuizAttemptCommandValidator
+        : AbstractValidator<CreateQuizAttemptCommand>
+    {
+        public CreateQuizAttemptCommandValidator()
+        {
+            RuleFor(x => x.StudentId)
+                .NotEmpty();
+
+            RuleFor(x => x.QuizId)
+                .NotEmpty();
+
+            RuleFor(x => x.StartTime)
+                .NotEmpty();
+
+            RuleFor(x => x.Deadline)
+                .GreaterThan(x => x.StartTime);
+
+            RuleFor(x => x.attemptStatus)
+                .Equal(AttemptStatus.InProgress);
+        }
+    }
+}
