b5f9bb2df07c8e665f63e07a31c86760ca5fdab6
Rojena Shehata <rojenashehata@gmail.com>
2026-09-13 00:47:08 +0300

    feat(attempt): add start attempt request and response DTOs and viewmodels

---
+++ Features/Attempts/StartAttempt/ViewModels/AttemptQuestionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using exam_system.Features.Attempts.StartAttempt.DTOs;
+
+namespace exam_system.Features.Attempts.StartAttempt.ViewModels
+{
+    public class AttemptQuestionViewModel
+    {
+        public Guid Id { get; init; }
+
+        public string Text { get; init; } = string.Empty;
+        public List<AttemptOptionViewModel> Options { get; set; } = [];
+    }
+}
