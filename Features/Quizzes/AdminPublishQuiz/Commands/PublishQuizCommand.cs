4ba6372c4e35332cb150cd58339e78c9c5c6b162
madinetmasr <shehabsameh.shebo@gmail.com>
2026-09-13 22:13:28 +0300

    publish quiz

---
+++ Features/Quizzes/AdminPublishQuiz/Commands/PublishQuizCommand.cs
@@ -0,0 +1,9 @@
+﻿using exam_system.Features.Shared;
+using MediatR;
+
+namespace exam_system.Features.Quizzes.AdminPublishQuiz.Commands
+{
+    public record PublishQuizCommand(Guid QuizId) : IRequest<RequestResponse<bool>>;
+
+
+}
---
+++ Features/Quizzes/AdminPublishQuiz/Controllers/PublishQuizController.cs
@@ -0,0 +1,25 @@
+﻿using exam_system.Features.Quizzes.AdminPublishQuiz.Orchestrators;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exam_system.Features.Quizzes.AdminPublishQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize("Admin")]
+    public class PublishQuizController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("{quizId}/publish")]
+        public async Task<IActionResult> Publish(
+    Guid quizId,
+    CancellationToken cancellationToken)
+        {
+           var result = await mediator.Send(
+                new PublishQuizOrchestrator(quizId),
+                cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
