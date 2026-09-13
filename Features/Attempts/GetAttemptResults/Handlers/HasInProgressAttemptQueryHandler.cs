c7d2fd39fa810a3fb2d1563e1895e1d88c117b38
madinetmasr <shehabsameh.shebo@gmail.com>
2026-09-13 22:24:21 +0300

    unpublish

---
+++ Features/Attempts/GetAttemptResults/Handlers/HasInProgressAttemptQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using exam_system.Common.Enums;
+using exam_system.Domain.Entities.Attempts;
+using exam_system.Features.Attempts.GetAttemptResults.Queries;
+using exam_system.Features.Shared;
+using exam_system.Persistence.DataAccess;
+using MediatR;
+
+namespace exam_system.Features.Attempts.GetAttemptResults.Handlers
+{
+    //public class HasInProgressAttemptQueryHandler(IGenericRepository<QuizAttempt> repository) : IRequestHandler<HasInProgressAttemptQuery, RequestResponse<bool>>
+    //{
+       
+
+    //    //Task<RequestResponse<bool>> IRequestHandler<HasInProgressAttemptQuery, RequestResponse<bool>>.Handle(HasInProgressAttemptQuery request, CancellationToken cancellationToken)
+    //    //{
+    //    //    var HasInProgressAttempt = repository.GetAll().Any(q=>q.);
+    //    //}
+    //}
+}
